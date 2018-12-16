21aef8eb2a81f31eb7e18ca5baff0285540dde20
Mateusz Komorowski <matijerzykom@op.pl>
2018-12-16 11:50:30 +0100

    First commit

---
+++ Point2D.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Threading.Tasks;
+
+class Point2D
+{
+    /*********************************************
+    * Private variables
+    *********************************************/
+    //Two coordinates of the point
+    private SqlDouble x;
+    private SqlDouble y;
+
+
+    /*********************************************
+     * Public accessors
+     *********************************************/
+    public SqlDouble X
+    {
+        get => x;
+    }
+    public SqlDouble Y
+    {
+        get => y;
+    }
+
+    /*********************************************
+     * Public functions
+     *********************************************/
+    // main constructor
+    public Point2D(SqlDouble x, SqlDouble y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    //secondary constructor with single input
+    public Point2D(SqlDouble coor)
+    {
+        x = coor;
+        y = coor;
+    }
+
+    //secondary constructor with single input
+    public Point2D(SqlString coords)
+    {
+        string[] strcords = coords.ToString().Split(',');
+
+        x = SqlDouble.Parse(strcords[0]);
+        y = SqlDouble.Parse(strcords[1]);
+    }
+
+    //get distance between two points
+    public static SqlDouble getDistance(Point2D p1, Point2D p2)
+    {
+        return Math.Sqrt(Math.Pow((double)(p2.Y-p1.Y), 2)+Math.Pow((double)(p2.X-p1.X), 2));
+    }
+
+    /*********************************************
+     * Private utility functions
+     *********************************************/
+
+
+    /*********************************************
+     * Override functions
+     *********************************************/
+     // Use StringBuilder to provide string representation of UDT.  
+    public override string ToString()
+    {
+        return "("+X+';'+Y+") ";
+    }
+}
+
---
+++ Polygon.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+using System.Text;
+using System.Collections.Generic;
+
+[Serializable]
+[Microsoft.SqlServer.Server.SqlUserDefinedType(Format.Native,
+    IsByteOrdered = true,
+    ValidationMethodName = "ValidatePoint")]
+public class Polygon
+{
+    /*********************************************
+     * Private variables
+     *********************************************/
+    //The number of lines of the shape
+    private SqlInt32 lines;
+
+    //Array of points
+    private List<Point2D> points;
+
+    /*********************************************
+     * Public accessors
+     *********************************************/
+    public SqlInt32 Lines
+    {
+        get => lines;
+    }
+    /*********************************************
+     * Public functions
+     *********************************************/
+    //default constructor
+    public Polygon(SqlString coordinates)
+    {
+        points = new List<Point2D>();
+        string[] strcoords = coordinates.ToString().Split(';');
+        lines = strcoords.Length;
+        foreach(string coord in strcoords)
+        {
+            points.Add(new Point2D(coord));
+        }
+    }
+
+
+    /*********************************************
+     * Private utility functions
+     *********************************************/
+    //field
+    public SqlDouble getField()
+    {
+          // Initialze area 
+        SqlDouble area = 0.0; 
+  
+        // Calculate value of shoelace formula 
+        int j = (int)lines - 1;
+
+        for (int i = 0; i < lines; i++)
+        {
+            area += (points[j].X + points[i].X) * (points[j].Y - points[i].Y);
+        }
+            return area;
+    }
+
+    //Circuit
+    public SqlDouble getCircuit()
+    {
+        SqlDouble circuit = 0.0;
+        for (int i = 1; i<lines; i++)
+        {
+            circuit +=Point2D.getDistance(points[i-1], points[i]);
+        }
+        return circuit;
+    }
+
+    /*********************************************
+     * Override functions
+     *********************************************/
+
+    // Use StringBuilder to provide string representation of UDT.  
+    public override string ToString()
+    {
+        string text = "Polygon of "+lines+" lines, with points: ";
+        foreach(Point2D points in points)
+        {
+            text += points.ToString();
+        }
+        return text+'\n';
+    }
+
+}
+
